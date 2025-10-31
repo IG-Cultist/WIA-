6e643067046fcd16313219a7a855da17b0d872c7
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2025-10-31 12:11:59 +0900

    GimmickSceneを編集。FlowerPotスクリプト、MoveArmスクリプトを編集。植木鉢のプレハブを編集。植木鉢の破片のプレハブを編集。クレーンをプレハブ化。ステージ1のシーンをコピーして自分のフォルダにペースト、名称を変更

--- WIA.Client/Assets/99_Users/10_Miura/02_Script/MoveArm.cs
+++ WIA.Client/Assets/99_Users/10_Miura/02_Script/MoveArm.cs
@@ -15,9 +15,9 @@ public class MoveArm : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; //水平方向の移動
+        float moveZ = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime; //水平方向の移動
 
-        transform.position += new Vector3(moveX, 0, 0); // オブジェクトの位置を更新
+        transform.position += new Vector3(0, 0, moveZ); // オブジェクトの位置を更新
 
     }
 }
