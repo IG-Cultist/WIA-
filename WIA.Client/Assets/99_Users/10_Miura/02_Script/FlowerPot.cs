db7c0d887133749569e97699933ce8c4cec3891c
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2025-10-31 09:48:24 +0900

    植木鉢のプレハブを編集。植木鉢の破片のプレハブを編集。FlowerPotスクリプトを編集

--- WIA.Client/Assets/99_Users/10_Miura/02_Script/FlowerPot.cs
+++ WIA.Client/Assets/99_Users/10_Miura/02_Script/FlowerPot.cs
@@ -25,7 +25,29 @@ public class FlowerPot : MonoBehaviour
         //A–Ø”«‚ğÁ‚·
         Destroy(potObj);
 
+
+        for (int i = 0; i < potFragmentObj.transform.childCount; i++)
+        {//potFragmentObj‚Ìq‚Ì”‚¾‚¯ƒ‹[ƒv
+
+            potFragmentObj.transform.GetChild(i).GetComponent<Rigidbody>().AddForce(new Vector2(1000, 200)); //‰E‘¤‚É”j•Ğ‚ğ”ò‚Î‚·
+
+            FadeFragment(potFragmentObj.transform.GetChild(i));
+        }
+
         //A–Ø”«‚Ì”j•Ğ‚ğ¶¬‚·‚é
         Instantiate(potFragmentObj, this.transform.position, this.transform.rotation);
+    }
+
+    public void FadeFragment(Transform fragment)
+    {
+        fragment.GetComponent<Renderer>().material.DOFade(0, 6);
+
+        //DestroyFragment(fragment);
+    }
+
+    public async void DestroyFragment(Transform fragment)
+    {
+        await Task.Delay(6000);
+        Destroy(fragment.gameObject);
     }
 }
