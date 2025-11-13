1a8e40064231d0c239925fe64ad2ad45dc4b7b4b
川口京佑 <231076@st.yoshida.-g.ac.jp>
2025-11-13 10:44:46 +0900

    復旧

--- WIA.Client/Assets/99_Users/04_Kawaguchi/02_Script/30_Result/RadarChart.cs
+++ WIA.Client/Assets/99_Users/04_Kawaguchi/02_Script/30_Result/RadarChart.cs
@@ -9,22 +9,22 @@ public class RadarChart : MonoBehaviour
     /// <summary>
     /// 基準数値
     /// </summary>
-    private Vector3 baseCruel = new Vector3(0f,13.5f,0f);   
+    private Vector3 baseCruel = new Vector3(0f, 13.5f, 0f);
     private Vector3 baseJudge = new Vector3(13.3f, 3.7f, 0f);
     private Vector3 baseCarelessly = new Vector3(8.4f, -12f, 0f);
-    private Vector3 basePlan = new Vector3(-8.4f, -12f, 0f); 
+    private Vector3 basePlan = new Vector3(-8.4f, -12f, 0f);
     private Vector3 baseCool = new Vector3(-13.3f, 3.7f, 0f);
 
     private float point = 0.5f;
 
-    public void DrawRadar()
+    public void SetRadarChart(float[] radarList)
     {
         ///数値変更(減法方式で)
-        positions[0] = baseCruel * point;
-        positions[1] = baseJudge * point;
-        positions[2] = baseCarelessly * point;
-        positions[3] = basePlan * point;
-        positions[4] = baseCool * point;
+        positions[0] = baseCruel * radarList[0];
+        positions[1] = baseJudge * radarList[1];
+        positions[2] = baseCarelessly * radarList[2];
+        positions[3] = basePlan * radarList[3];
+        positions[4] = baseCool * radarList[4];
 
         lineRenderer.positionCount = positions.Length;
         lineRenderer.SetPositions(positions);
@@ -34,12 +34,12 @@ public class RadarChart : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        DrawRadar();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //DrawRadar();
     }
 }
