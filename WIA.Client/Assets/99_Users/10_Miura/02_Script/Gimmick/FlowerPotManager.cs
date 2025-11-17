5fe35ff69ec9122a746c2e4ae09506d16542330a
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2025-11-17 10:51:43 +0900

    GimmickScene、Stage_1_Mシーンを編集。Baseタグを追加。UISceneを追加。SceneLoaderスクリプトを追加。フェードのアニメーションとコントローラーを削除

--- WIA.Client/Assets/99_Users/10_Miura/02_Script/Gimmick/FlowerPotManager.cs
+++ WIA.Client/Assets/99_Users/10_Miura/02_Script/Gimmick/FlowerPotManager.cs
@@ -11,7 +11,6 @@ public class FlowerPotManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
