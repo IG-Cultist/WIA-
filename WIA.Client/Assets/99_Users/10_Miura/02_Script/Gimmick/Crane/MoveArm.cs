97a249125365b1d606428fa544cff628c21f6ce9
川口京佑 <231076@st.yoshida.-g.ac.jp>
2025-11-19 10:01:42 +0900

    クレーンコンテナの挙動変更

--- WIA.Client/Assets/99_Users/10_Miura/02_Script/Gimmick/Crane/MoveArm.cs
+++ WIA.Client/Assets/99_Users/10_Miura/02_Script/Gimmick/Crane/MoveArm.cs
@@ -18,11 +18,11 @@ public class MoveArm : MonoBehaviour
 
         if (Input.GetKey(KeyCode.R))
         {
-            armTransform.DORotate(new Vector3(0, 35, 0), 8);
+            armTransform.DORotate(new Vector3(0, 45, 0), 30);
         }
         if (Input.GetKey(KeyCode.L))
         {
-            armTransform.DORotate(new Vector3(0, -35, 0), 8);
+            armTransform.DORotate(new Vector3(0, -45, 0), 30);
         }
     }
 }
