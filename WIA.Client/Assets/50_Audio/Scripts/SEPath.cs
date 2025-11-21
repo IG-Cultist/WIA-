9a7d799456f209f31f34533bf11951b1c09b1fb3
川口京佑 <231076@st.yoshida.-g.ac.jp>
2025-11-21 10:13:37 +0900

    罹災者プレイヤーのプレイヤー角度同期を修正・コンテナにSE追加・モーションブラー追加(後から調整予定)

--- WIA.Client/Assets/50_Audio/Scripts/SEPath.cs
+++ WIA.Client/Assets/50_Audio/Scripts/SEPath.cs
@@ -5,14 +5,15 @@
 /// </summary>
 public static class SEPath{
 
-	public const string DEATH        = "SE/Death";
-	public const string DEATH_VOICE  = "SE/Voice/Death_Voice";
-	public const string ERROR        = "SE/Error";
-	public const string FALL         = "SE/Fall";
-	public const string FOCUS_BUTTON = "SE/Focus_Button";
-	public const string GAME_START   = "SE/Game_Start";
-	public const string NEXT_PAGE    = "SE/NextPage";
-	public const string PUSH_BUTTON  = "SE/Push_Button";
+	public const string CONTAINER_SOUND = "SE/Container_Sound";
+	public const string DEATH           = "SE/Death";
+	public const string DEATH_VOICE     = "SE/Voice/Death_Voice";
+	public const string ERROR           = "SE/Error";
+	public const string FALL            = "SE/Fall";
+	public const string FOCUS_BUTTON    = "SE/Focus_Button";
+	public const string GAME_START      = "SE/Game_Start";
+	public const string NEXT_PAGE       = "SE/NextPage";
+	public const string PUSH_BUTTON     = "SE/Push_Button";
 
 }
 
