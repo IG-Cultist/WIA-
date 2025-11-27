2cb8617d44bf7e4c743d061a97b5ea55d56a5032
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2025-11-27 12:21:06 +0900

    Stage_2シーンを編集。FlowerPotスクリプト、FlowerPotManagerスクリプト、FlowerPot_objプレハブを編集

--- WIA.Client/Assets/99_Users/10_Miura/02_Script/Gimmick/FlowerPot/FlowerPotManager.cs
+++ WIA.Client/Assets/99_Users/10_Miura/02_Script/Gimmick/FlowerPot/FlowerPotManager.cs
@@ -13,32 +13,29 @@ public class FlowerPotManager : MonoBehaviour
 
     public List<GameObject> potList = new List<GameObject>(); //A–Ø”«‚Ì¶¬ŒÂ”‚ğŠi”[‚·‚éƒŠƒXƒg
     public List<GameObject> randomSpawnPoint =new List<GameObject>(); //A–Ø”«‚ªƒXƒ|[ƒ“‚·‚éêŠ‚ÌƒŠƒXƒg
+    public List<int> nowSpawnList=new List<int>();
 
     public bool isThreePot = false; //A–Ø”«‚ª3ŒÂ‘¶İ‚·‚é‚©‚Ç‚¤‚©‚Ì•Ï”
+    public int generatNumber;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        //A–Ø”«‚ğ¶¬‚·‚é
+        GeneratePot();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Return))
-        {//EnterƒL[‚ğ‰Ÿ‚³‚ê‚½‚ç
-            if (isThreePot == true) { return; } //A–Ø”«‚ª3ŒÂˆÈã‚ ‚éê‡AˆÈ‰º‚Ìˆ—‚ğÀs‚µ‚È‚¢
-
+        if (potList.Count >= 3)
+        {//potList‚Ì—v‘f‚ª3ˆÈã‚¾‚Á‚½‚ç
+            isThreePot = true; // A–Ø”«‚ª3ŒÂ‚ ‚éó‘Ô‚É‚·‚é
+        }
+        if (potList.Count <= 2)
+        {//potList‚Ì—v‘f‚ª2ˆÈ‰º‚¾‚Á‚½‚ç
             GeneratePot(); //A–Ø”«‚ğ¶¬‚·‚é
-
-            if (potList.Count >= 3)
-            {//potList‚Ì—v‘f‚ª3ˆÈã‚¾‚Á‚½‚ç
-                isThreePot = true; // A–Ø”«‚ª3ŒÂ‚ ‚éó‘Ô‚É‚·‚é
-            }
-            if (potList.Count <= 2)
-            {//potList‚Ì—v‘f‚ª2ˆÈ‰º‚¾‚Á‚½‚ç
-                isThreePot = false; // A–Ø”«‚ª3ŒÂ‘¶İ‚µ‚È‚¢ó‘Ô
-            }
+            isThreePot = false; // A–Ø”«‚ª3ŒÂ‘¶İ‚µ‚È‚¢ó‘Ô
         }
     }
 
@@ -47,19 +44,30 @@ public class FlowerPotManager : MonoBehaviour
     /// </summary>
     private void GeneratePot()
     {
-        if(isThreePot==false)
-        {//A–Ø”«‚ªOŒÂ–³‚¢
-            for (int i = 0; i < 3; i++)
-            {//O‰ñ•ªŒJ‚è•Ô‚·
-                // ¶¬ˆÊ’u‚ğŒˆ‚ß‚é
-                int generatNumber = Random.Range(0, randomSpawnPoint.Count); // ¶¬ˆÊ’u‚ğspawnPointPos‚Ì’†‚©‚çŒˆ‚ß‚é
+        while (true)
+        {        
+            // ¶¬ˆÊ’u‚ğŒˆ‚ß‚é
+            generatNumber = Random.Range(0, randomSpawnPoint.Count); // ¶¬ˆÊ’u‚ğspawnPointPos‚Ì’†‚©‚çŒˆ‚ß‚é
 
-                    // potObj‚ğ¶¬‚·‚é
-                    Instantiate(potObj, randomSpawnPoint[generatNumber].transform.position, randomSpawnPoint[generatNumber].transform.rotation); // randomSpawnPoint‚ÉŠi”[‚³‚ê‚½gameObject‚ÌgeneratNumber‚ÌêŠ‚É¶¬
-
-                //potList‚É—v‘f‚ğ’Ç‰Á‚·‚é
-                    potList.Add(potObj);
+            if (!nowSpawnList.Contains(generatNumber))
+            {
+                break;
             }
+        }
+        nowSpawnList.Add(generatNumber);
+
+        // potObj‚ğ¶¬‚·‚é
+        Instantiate(potObj, randomSpawnPoint[generatNumber].transform.position, randomSpawnPoint[generatNumber].transform.rotation); // randomSpawnPoint‚ÉŠi”[‚³‚ê‚½gameObject‚ÌgeneratNumber‚ÌêŠ‚É¶¬
+
+        //potList‚É—v‘f‚ğ’Ç‰Á‚·‚é
+        potList.Add(potObj);
+    }
+
+    public void RemoveList(int generateNumber)
+    {
+        if(nowSpawnList.Contains(generateNumber))
+        {
+            nowSpawnList.Remove(generateNumber);
         }
     }
 }
