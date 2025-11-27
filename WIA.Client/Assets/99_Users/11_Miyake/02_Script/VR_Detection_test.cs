4289f54b3700ce57278efb1f27a96eaae852ee2c
S-JoBi23-058\student <231064@st.yoshida-g.ac.jp>
2025-11-27 11:39:08 +0900

    VRを使用していることを検知する処理実装

--- WIA.Client/Assets/99_Users/11_Miyake/02_Script/VR_Detection_test.cs
+++ WIA.Client/Assets/99_Users/11_Miyake/02_Script/VR_Detection_test.cs
@@ -1,31 +1,62 @@
-using Unity.XR.OpenVR;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.XR;
 
 public class VR_Detection : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static VR_Detection Instance;
+
+    public bool isVRActive = false;
+    public string deviceName = "None";
+
+    private void Awake()
     {
-        DetectCurrentDevices();
+        // シングルトン化（必要ない場合は削除OK）
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    void DetectCurrentDevices()
+    void Start()
     {
-        Debug.Log("接続中のデバイス一覧:");
+        DetectVR();
+    }
 
-        foreach (var device in InputSystem.devices)
+    void Update()
+    {
+        // 実行中にVR機器の接続状況が変わることがある場合に再確認
+        DetectVR();
+    }
+
+    void DetectVR()
+    {
+        isVRActive = XRSettings.isDeviceActive;
+        deviceName = XRSettings.loadedDeviceName;
+
+        if (isVRActive)
         {
-            Debug.Log($"デバイス名: {device.name}, クラス: {device.GetType().Name}");
-
-            if (device is Keyboard)
-            {
-                Debug.Log("これはキーボードです。");
-            }
-            else if (device is OpenVRHMD)
-            {
-                Debug.Log("これはVRです。");
-            }
+            VRMode();
         }
+        else
+        {
+            NonVRMode();
+        }
+    }
+
+    void VRMode()
+    {
+        // ここにVR専用処理を書く
+        Debug.Log("【VRモード】使用中のデバイス：" + deviceName);
+    }
+
+    void NonVRMode()
+    {
+        // ここにPC・スマホ用処理を書く
+        Debug.Log("【非VRモード】現在VRは使用されていません");
     }
 }
