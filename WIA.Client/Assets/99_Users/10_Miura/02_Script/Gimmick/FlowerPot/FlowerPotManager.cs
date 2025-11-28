9f86bc12bc3084887845fe690eb22e8ce78272c5
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2025-11-28 11:54:57 +0900

    Stage_2シーンを編集。FlowerPotManagerスクリプトを編集

--- WIA.Client/Assets/99_Users/10_Miura/02_Script/Gimmick/FlowerPot/FlowerPotManager.cs
+++ WIA.Client/Assets/99_Users/10_Miura/02_Script/Gimmick/FlowerPot/FlowerPotManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 using static UnityEditorInternal.ReorderableList;
 using UnityEngine.UIElements;
 
-
 public class FlowerPotManager : MonoBehaviour
 {
     [SerializeField] public GameObject potObj; //植木鉢オブジェクト
@@ -31,6 +30,7 @@ public class FlowerPotManager : MonoBehaviour
         if (potList.Count >= 3)
         {//potListの要素が3以上だったら
             isThreePot = true; // 植木鉢が3個ある状態にする
+            RemoveList(generatNumber);
         }
         if (potList.Count <= 2)
         {//potListの要素が2以下だったら
