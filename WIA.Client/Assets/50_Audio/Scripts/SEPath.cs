73cee0dda2a3eb7b3f8f4695ad23d4bb043fb698
川口京佑 <231076@st.yoshida.-g.ac.jp>
2025-12-03 10:31:05 +0900

    効果音ファイル追加

--- WIA.Client/Assets/50_Audio/Scripts/SEPath.cs
+++ WIA.Client/Assets/50_Audio/Scripts/SEPath.cs
@@ -14,11 +14,15 @@ public static class SEPath{
 	public const string FIND_KEY        = "SE/Find_Key";
 	public const string FOCUS_BUTTON    = "SE/Focus_Button";
 	public const string GAME_START      = "SE/Game_Start";
+	public const string GET_KEY         = "SE/Get_Key";
+	public const string GLASS_CRASH     = "SE/Glass_Crash";
 	public const string IRON_HIT        = "SE/Iron_Hit";
 	public const string NEXT_PAGE       = "SE/NextPage";
 	public const string PUSH_BUTTON     = "SE/Push_Button";
 	public const string SEARCH_KEY      = "SE/Search_Key";
 	public const string SUCCESS         = "SE/Success";
+	public const string TASK_COMPLETED  = "SE/Task_Completed";
+	public const string TASK_FAILURE    = "SE/Task_Failure";
 	public const string UNLOCK_KEY      = "SE/Unlock_Key";
 	public const string WOOD_HIT        = "SE/Wood_Hit";
 
