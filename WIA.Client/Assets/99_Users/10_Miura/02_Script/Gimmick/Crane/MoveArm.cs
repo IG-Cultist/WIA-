614ecb783820c1c0c0c2cefad03ed6afdba1d209
S-JoBi23-058\student <231064@st.yoshida-g.ac.jp>
2025-12-17 14:02:05 +0900

    レバー関連のスクリプトの修正

--- WIA.Client/Assets/99_Users/10_Miura/02_Script/Gimmick/Crane/MoveArm.cs
+++ WIA.Client/Assets/99_Users/10_Miura/02_Script/Gimmick/Crane/MoveArm.cs
@@ -1,46 +1,37 @@
 using DG.Tweening;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class MoveArm : MonoBehaviour
 {
     [SerializeField] GameObject crane;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
     }
-
     // Update is called once per frame
     void Update()
     {
         Transform armTransform = crane.transform;
-
-        if (RoomModel.Instance && OnlineGameManager.Player.name == "Worker")
+        var keyBoardCurrent = Keyboard.current;
+        var lKey = keyBoardCurrent.lKey; //Lキー
+        var rKey = keyBoardCurrent.rKey; //Rキー
+        if (rKey.wasPressedThisFrame)
         {
-
+            armTransform.DORotate(new Vector3(0, 45, 0), 40);
         }
-        else if (RoomModel.Instance && OnlineGameManager.Player.name == "Stricker")
+        if (lKey.wasPressedThisFrame)
         {
-            if (Input.GetKey(KeyCode.R))
-            {
-                armTransform.DORotate(new Vector3(0, 45, 0), 40);
-            }
-            if (Input.GetKey(KeyCode.L))
-            {
-                armTransform.DORotate(new Vector3(0, -45, 0), 40);
-            }
-        }
-        else
-        {
-            if (Input.GetKey(KeyCode.R))
-            {
-                armTransform.DORotate(new Vector3(0, 45, 0), 40);
-            }
-            if (Input.GetKey(KeyCode.L))
-            {
-                armTransform.DORotate(new Vector3(0, -45, 0), 40);
-            }
+            armTransform.DORotate(new Vector3(0, -45, 0), 40);
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
