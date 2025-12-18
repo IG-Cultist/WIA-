13f1aebb610e8a1f2159ae6180767a5fc6738373
KosukeKida <231122@st.yoshida-g.ac.jp>
2025-12-18 11:27:25 +0900

    ステージ3まで出来るように実装

--- WIA.Client/Assets/99_Users/10_Miura/02_Script/Gimmick/Crane/Lever.cs
+++ WIA.Client/Assets/99_Users/10_Miura/02_Script/Gimmick/Crane/Lever.cs
@@ -6,6 +6,8 @@ public class Lever : MonoBehaviour
 {
     [SerializeField] GameObject leverObj;
     [SerializeField] GameObject craneObj;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,11 +26,27 @@ public class Lever : MonoBehaviour
         var rKey = keyBoardCurrent.rKey;
         if (lKey.wasPressedThisFrame)
         {//Lキーを押したら
-            leverTransform.DORotate(new Vector3(-20, armTransform.rotation.y, 0), 3); //レバーが左に傾く
+            if(RoomModel.Instance)
+            {
+                if (OnlineGameManager.Player.name == "Stricker")
+                    leverTransform.DORotate(new Vector3(-20, armTransform.rotation.y, 0), 3); //レバーが左に傾く
+            }
+            else
+            {
+                if (OnlineGameManager.Player.name == "Stricker")
+                    leverTransform.DORotate(new Vector3(-20, armTransform.rotation.y, 0), 3); //レバーが左に傾く
+            }
         }
         if (rKey.wasPressedThisFrame)
         {//Rキーを押したら
-            leverTransform.DORotate(new Vector3(20, armTransform.rotation.y, 0), 3); //レバーが右に傾く
+            if (RoomModel.Instance)
+            {
+                leverTransform.DORotate(new Vector3(20, armTransform.rotation.y, 0), 3); //レバーが右に傾く
+            }
+            else
+            {
+                leverTransform.DORotate(new Vector3(20, armTransform.rotation.y, 0), 3); //レバーが右に傾く
+            }
         }
 
         
