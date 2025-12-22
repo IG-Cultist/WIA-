a494db689c43d44e64dc97c913e86daa1f1f3721
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2025-12-22 14:14:52 +0900

    Revert "Stage_1シーンを編集。Player、containerスクリプトを編集。InputSystemActionsスクリプトを追加。Drag、InputSystemスクリプトを削除。"
    This reverts commit 47d10df9c9736ec481520cdbfea6e1bd37d908f2.

--- WIA.Client/Assets/99_Users/04_Kawaguchi/02_Script/container.cs
+++ WIA.Client/Assets/99_Users/04_Kawaguchi/02_Script/container.cs
@@ -10,44 +10,12 @@ public class container : MonoBehaviour
     [SerializeField] float m_radius ;
     [SerializeField] float m_upwards ;
     Vector3 m_position;
-    public Vector3 velocity;
-
-    // 1フレーム前の位置
-    private Vector3 _prevPosition;
-
-    public float nowSpeed = 0;
-
-    private void Start()
-    {
-        // 初期位置を保持
-        m_position = transform.position;
-    }
 
     void Update()
     {
 
     }
 
-    void FixedUpdate()
-    {
-        //現在の位置
-        var position = transform.position;
-
-        //現在の速度を計算
-        velocity = (position - _prevPosition) / Time.fixedDeltaTime;
-
-        //微小な誤差は無視
-        if (velocity.sqrMagnitude < 0.0001f)
-            velocity = Vector3.zero;
-
-        //前フレームの位置を更新
-        _prevPosition = position;
-
-        //nowSpeedに速さを代入
-        nowSpeed = velocity.magnitude;
-
-        Debug.Log(nowSpeed);
-    }
     private void OnCollisionEnter(Collision collision)
     {
        if (collision.gameObject.tag == "Player")
@@ -60,10 +28,6 @@ public class container : MonoBehaviour
     {
         /*m_particle.Play();
         m_position = m_particle.transform.position;*/
-
-        // deltaTimeが0の場合は何もしない
-        if (Mathf.Approximately(Time.deltaTime, 0))
-            return;
 
         // 範囲内のRigidbodyにAddExplosionForce
         Collider[] hitColliders = Physics.OverlapSphere(m_position, m_radius);
@@ -85,16 +49,19 @@ public class container : MonoBehaviour
                 Debug.Log("Playerにぶつかった！");
 
                 var rb = hitColliders[i].GetComponent<Rigidbody>();
-                if (rb&&nowSpeed>=5)
+                if (rb)
                 {
+
+
                     //吹き飛ばす方向を求める(プレイヤーから触れたものの方向)
-                    Vector3 toVec = GetAngleVec(hitColliders[i].gameObject, this.gameObject);
+                    Vector3 toVec = GetAngleVec(hitColliders[i].gameObject,this.gameObject);
 
                     //Y方向を足す
                     toVec = toVec + new Vector3(0, m_upwards, 0);
 
                     //ふきとべええ
                     rb.AddForce(toVec * m_force, ForceMode.Impulse);
+
                 }
 
             }
