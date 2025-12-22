afaebeb2d92f52fc41182a998ee25061ab007bb0
KosukeKida <231122@st.yoshida-g.ac.jp>
2025-12-22 16:17:09 +0900

    クレーン修正。ビルド環境仮確定

--- WIA.Client/Assets/99_Users/05_Kida/02_Script/MatchingManager.cs
+++ WIA.Client/Assets/99_Users/05_Kida/02_Script/MatchingManager.cs
@@ -273,15 +273,15 @@ public class MatchingManager : MonoBehaviour
         Initiate.DoneFading();
         if (RoomModel.Instance.joinedUserList[RoomModel.Instance.ConnectionId].JoinOrder == 1)
         {//ì≠Ç≠êl
-            //Initiate.Fade("Exp_Worker_K01", endColor, 2.0f);
+            Initiate.Fade("Exp_Worker_K01", endColor, 2.0f);
             //Initiate.Fade("Exp_Worker_K02", endColor, 2.0f);
-            Initiate.Fade("Exp_Worker_K03", endColor, 2.0f);
+            //Initiate.Fade("Exp_Worker_K03", endColor, 2.0f);
         }
         else if (RoomModel.Instance.joinedUserList[RoomModel.Instance.ConnectionId].JoinOrder == 2)
         {//òJç–ë§
-            //Initiate.Fade("Exp_Stricker_K01", endColor, 2.0f);
+            Initiate.Fade("Exp_Stricker_K01", endColor, 2.0f);
             //Initiate.Fade("Exp_Stricker_K02", endColor, 2.0f);
-            Initiate.Fade("Exp_Stricker_K03", endColor, 2.0f);
+            //Initiate.Fade("Exp_Stricker_K03", endColor, 2.0f);
         }
 
     }
