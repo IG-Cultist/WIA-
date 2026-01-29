4b695c7e075f1d0c5daffec0c3ee2c370b30ed03
KosukeKida <231122@st.yoshida-g.ac.jp>
2026-01-29 15:15:55 +0900

    死亡時に遷移できてなかったので修正

--- WIA.Client/Assets/99_Users/10_Miura/02_Script/TaskCheck.cs
+++ WIA.Client/Assets/99_Users/10_Miura/02_Script/TaskCheck.cs
@@ -71,12 +71,14 @@ public class TaskCheck : MonoBehaviour
 
                 if (RoomModel.Instance.joinedUserList[RoomModel.Instance.ConnectionId].JoinOrder == 1 && !isDead)
                 {
-                    if (UnityEngine.XR.XRSettings.isDeviceActive) Initiate.Fade("01_Exp_Worker_2_VR", Color.black, 1.0f);
+                    if (UnityEngine.XR.XRSettings.isDeviceActive) //Initiate.Fade("01_Exp_Worker_2_VR", Color.black, 1.0f);
+                        Initiate.Fade("VR_30_ResultScene", Color.black, 1.0f);
                     else Initiate.Fade("01_Exp_Worker_2", Color.black, 1.0f);
                 }
                 else if (RoomModel.Instance.joinedUserList[RoomModel.Instance.ConnectionId].JoinOrder == 2 && !isDead) 
                 {
-                    if (UnityEngine.XR.XRSettings.isDeviceActive) Initiate.Fade("01_Exp_Stricker_2_VR", Color.black, 1.0f);
+                    if (UnityEngine.XR.XRSettings.isDeviceActive) //Initiate.Fade("01_Exp_Stricker_2_VR", Color.black, 1.0f);
+                        Initiate.Fade("VR_30_ResultScene", Color.black, 1.0f);
                     else Initiate.Fade("01_Exp_Stricker_2", Color.black, 1.0f);
                 } 
             }
